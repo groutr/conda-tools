2925e414d7545133429c23b1c342d59867aacddd
Ryan Grout <ayr035@gmail.com>
2016-06-06 11:29:14 -0500

    add convenience methods for loading an entire package cache
    Files should be returned as a set

--- cache.py
+++ cache.py
@@ -2,15 +2,19 @@ import json
 import os
 from functools import lru_cache
 
+class InvalidCachePackage(Exception):
+    pass
 
 @lru_cache()
 class PackageInfo(object):
     def __init__(self, path):
         self.path = path
-        if os.path.exists(path):
-            self._info = os.path.join(path, 'info')
+        self._info = os.path.join(path, 'info')
+        if os.path.exists(path) and os.path.exists(self._info):
             self._index = os.path.join(self._info, 'index.json')
             self._files = os.path.join(self._info, 'files')
+        else:
+            raise InvalidCachePackage("{} does not exist".format(self._info))
 
     @lru_cache(maxsize=1)
     def index(self):
@@ -22,7 +26,7 @@ class PackageInfo(object):
     def files(self):
         with open(self._files, 'r') as f:
             x = map(str.strip, f.readlines())
-        return list(x)
+        return set(x)
 
     def __repr__(self):
         return 'PackageInfo({})'.format(self.path)
@@ -31,7 +35,7 @@ class PackageInfo(object):
         return '{}::{}'.format(self.index()['name'], self.index()['version'])
 
 
-def load_cache(path):
+def load_cache(path, verbose=False):
     if not os.path.exists(path):
         raise IOError('{} cache does not exist!'.format(path))
 
@@ -39,5 +43,19 @@ def load_cache(path):
     cache = os.walk(path, topdown=True)
     root, dirs, files = next(cache)
     for d in dirs:
-        result.append(PackageInfo(os.path.join(root, d)))
+        try:
+            result.append(PackageInfo(os.path.join(root, d)))
+        except InvalidCachePackage:
+            if verbose:
+                print("Skipping {}".format(d))
     return tuple(result)
+
+
+def named_cache(path):
+    """
+    Return dictionary of cache with (package name, package version) mapped to cache entry.
+    :param path: path to pkg cache
+    :return: dict
+    """
+    return {(i.index()['name'], i.index()['version']): i
+            for i in load_cache(path)}
