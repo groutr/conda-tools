b874b21a1ee5cc4a1c2391c21e78fd382438ad25
Ryan Grout <ayr035@gmail.com>
2016-06-06 15:44:55 -0500

    add config.py for configuration interface
    Configurations are JSON files that define paths for
    environments, package cache, and prefix

---
+++ config.py
@@ -0,0 +1,18 @@
+import json
+from os.path import exists
+
+config = None
+
+def load_config(path):
+    if exists(path):
+        with open(path, 'r') as cin:
+            x = json.load(cin)
+
+    global config
+    config = config or x
+
+def save_config(path, config):
+    with open(path, 'w') as cout:
+        json.dump(cout)
+
+
