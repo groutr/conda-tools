987d21e2af1d5b691b17e996d6d8d086d59aa26e
Ryan Grout <ayr035@gmail.com>
2016-07-11 17:29:16 -0500

    x

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(name='conda_tools',
       version='0.1',
       description='Useful abstractions of conda environments and package cache',
       author='Ryan Grout',
-      author_email='ryan@ryangrout.org',
       url='https://github.com/groutr/conda-tools',
       packages=['conda_tools']
 )
