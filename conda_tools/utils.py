ef136671c47b52a949135b98e84dae1d907952ce
Ryan Grout <ayr035@gmail.com>
2016-07-19 23:59:55 -0500

    do not follow symlinks
    Don't mask all errors, only os.error.

--- conda_tools/utils.py
+++ conda_tools/utils.py
@@ -1,4 +1,4 @@
-import os
+from os import lstat, error
 
 
 def is_hardlinked(f1, f2):
@@ -6,8 +6,8 @@ def is_hardlinked(f1, f2):
     Determine if two files are hardlinks to the same inode.
     """
     try:
-        s, d = os.stat(f1), os.stat(f2)
+        s, d = lstat(f1), lstat(f2)
         return s.st_ino == d.st_ino and s.st_dev == d.st_dev
-    except:
+    except error:
         return False
 
