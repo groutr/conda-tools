3f7851ba6af4422ae359a290d257d0435da4cfc6
Ryan Grout <ryan@ryangrout.org>
2018-11-14 23:12:29 -0600

    Update for src/ layout.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+import glob
 
 setup(name='conda_tools',
-      version='0.1',
-      description='Useful abstractions of conda environments and package cache',
-      author='Ryan Grout',
-      author_email='ryan@ryangrout.org',
-      url='https://github.com/groutr/conda-tools',
-      packages=['conda_tools']
+    version='0.1',
+    description='Useful abstractions of conda environments and package cache',
+    author='Ryan Grout',
+    author_email='ryan@ryangrout.org',
+    url='https://github.com/groutr/conda-tools',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    zip_safe=False      
 )
